56e78dc56345293b61ae54dd7eef840dcda605ce
Egor Petr <sportasd@gmail.com>
2021-02-17 15:58:21 +0700

    ver 002

--- program.py
+++ program.py
@@ -1,2 +1,13 @@
-print('Я домашка')
-print('Я домашка')
+class Product:
+    def __init__(self, name = '', kalories = 0, group = ''):
+        self.name = name
+        self.kalories = kalories
+        self.group = group
+    
+    def ShowProduct(self):
+        print(f'{self.name} is {self.group} and has {self.kalories} kalories')
+
+
+newpord1 = Product('Potatoe', 100, 'vegetables')
+
+newpord1.ShowProduct()
